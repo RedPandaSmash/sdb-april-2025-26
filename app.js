6d02175ea8e7e2cebeaf3b54932ce7fd53671a93
RedPandaSmash <redpandasmash@gmail.com>
2025-06-18 19:14:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,20 @@
 // import express from 'express';
 
 // commonjs syntax
-const express = require('express');
+const express = require("express");
 
-const app = express()
+const app = express();
 
 const PORT = 8080;
 
 app.get("/welcome", (req, res) => {
-    res.send("Happy Wednesday! No class tomorrow!")
-})
+  res.send("Happy Wednesday! No class tomorrow!");
+});
+
+app.get("/hippopotamus", (req, res) => {
+  res.send("Hippopotamus.");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
