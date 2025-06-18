108a7c50cf4b018eefcb2678673ae595eff5d9b9
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-18 18:14:20 -0500

    url encoded middleware

--- app.js
+++ app.js
@@ -4,14 +4,21 @@
 // commonjs syntax
 const express = require('express');
 
+// Create an instance of an Express application
 const app = express()
 
+// Define the port number
 const PORT = 8080;
 
+// Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+
+// GET - /welcome - Responds with a welcome message
 app.get("/welcome", (req, res) => {
     res.send("Happy Wednesday! No class tomorrow!")
 })
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
