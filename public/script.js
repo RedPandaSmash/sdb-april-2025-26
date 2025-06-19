985d5fb15807904027549ab499a732ecad26a153
RedPandaSmash <redpandasmash@gmail.com>
2025-06-19 17:34:07 -0400

    matching main

---
+++ public/script.js
@@ -0,0 +1,7 @@
+console.log("hi mom");
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", (event) => {
+  console.log("Button clicked!");
+});
